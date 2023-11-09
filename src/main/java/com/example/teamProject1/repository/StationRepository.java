388558cd3644ce30c4c9c90457811ddc4af93bb2
jimmy0524 <10jmin04@naver.com>
2023-11-09 18:31:52 +0900

    최소비용/최소시간/최단거리 알고리즘

---
+++ src/main/java/com/example/teamProject1/repository/StationRepository.java
@@ -0,0 +1,7 @@
+package com.example.teamProject1.repository;
+
+import com.example.teamProject1.model.Station;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StationRepository extends JpaRepository<Station,Integer> {
+}
