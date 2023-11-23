585599aef20c1b348c3ec5027c982654d068cc5f
jimmy0524 <10jmin04@naver.com>
2023-11-23 12:33:44 +0900

    SubwayApiController 수정

---
+++ src/main/java/com/example/teamProject1/controller/api/SubwayApiController.java
@@ -0,0 +1,24 @@
+package com.example.teamProject1.controller.api;
+
+import com.example.teamProject1.service.StationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+public class SubwayApiController {
+    @Autowired
+    StationService stationService;
+
+    @GetMapping("/subway/{start}/{end}")
+    public String subway(Model model, @PathVariable int start, @PathVariable int end) {
+        int cost = stationService.shortestPath(start, end, "cost");
+        int dist = stationService.shortestPath(start, end, "distance");
+        int time = stationService.shortestPath(start, end, "time");
+        model.addAttribute("cost",cost);
+        model.addAttribute("dist",dist);
+        model.addAttribute("time",time);
+        String result = "cost : " + cost +"dist : "+dist + "time : " + time;
+        return result;
+    }
+}
