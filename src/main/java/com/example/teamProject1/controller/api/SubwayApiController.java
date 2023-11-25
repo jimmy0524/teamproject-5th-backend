d16276d9ec349f433ca79861641332fe9c96ba85
Juuunyeok <fine5710@naver.com>
2023-11-26 00:15:35 +0900

    update: 역 이름으로 길찾기 기능 가능하게 변경

--- src/main/java/com/example/teamProject1/controller/api/SubwayApiController.java
+++ src/main/java/com/example/teamProject1/controller/api/SubwayApiController.java
@@ -1,25 +1,42 @@
 package com.example.teamProject1.controller.api;
 
+import com.example.teamProject1.Dto.SubwayLogicDto;
+import com.example.teamProject1.model.SubwayStation;
 import com.example.teamProject1.service.StationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SubwayApiController {
     @Autowired
     StationService stationService;
 
-    @GetMapping("/subway/{start}/{end}")
-    public String subway(Model model, @PathVariable int start, @PathVariable int end) {
-        int cost = stationService.shortestPath(start, end, "cost");
-        int dist = stationService.shortestPath(start, end, "distance");
-        int time = stationService.shortestPath(start, end, "time");
+//    @GetMapping("/subway/{start}/{end}")
+//    public String subway(Model model, @PathVariable int start, @PathVariable int end) {
+//        int cost = stationService.shortestPath(start, end, "cost");
+//        int dist = stationService.shortestPath(start, end, "distance");
+//        int time = stationService.shortestPath(start, end, "time");
+//        model.addAttribute("cost",cost);
+//        model.addAttribute("dist",dist);
+//        model.addAttribute("time",time);
+//        String result = "cost : " + cost +"dist : "+dist + "time : " + time;
+//        return result;
+//    }
+
+    @GetMapping("/api/subway")
+    public String subway(@RequestBody SubwayLogicDto subwayLogicDto, Model model) {
+        int cost = stationService.shortestPath(subwayLogicDto, "cost");
+        int dist = stationService.shortestPath(subwayLogicDto, "distance");
+        int time = stationService.shortestPath(subwayLogicDto, "time");
         model.addAttribute("cost",cost);
         model.addAttribute("dist",dist);
         model.addAttribute("time",time);
         String result = "cost : " + cost +"dist : "+dist + "time : " + time;
         return result;
     }
+
 }
--- src/main/java/com/example/teamProject1/service/StationService.java
+++ src/main/java/com/example/teamProject1/service/StationService.java
@@ -1,8 +1,11 @@
 package com.example.teamProject1.service;
 
+import com.example.teamProject1.Dto.SubwayLogicDto;
 import com.example.teamProject1.model.Station;
+import com.example.teamProject1.model.SubwayStation;
 import com.example.teamProject1.node.StationNode;
 import com.example.teamProject1.repository.StationRepository;
+import com.example.teamProject1.repository.SubwayStationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,18 +20,29 @@ public class StationService {
     @Autowired
     private StationRepository subwayRepository;
 
-    public int shortestPath(int start, int end, String type) {
+    @Autowired
+    private SubwayStationRepository subwayStationRepository;
+
+
+    public int shortestPath(SubwayLogicDto subwayLogicDto, String type) {
+
+        SubwayStation startSubwayStation = subwayStationRepository.findByName(subwayLogicDto.getStartStationName())
+                .orElseThrow(() -> new IllegalArgumentException("해당 역이 존재하지 않습니다."));
+
+        SubwayStation endSubwayStation = subwayStationRepository.findByName(subwayLogicDto.getEndStationName())
+                .orElseThrow(() -> new IllegalArgumentException("해당 역이 존재하지 않습니다."));
+
         List<Station> subways = subwayRepository.findAll();
         int n = 1000;
         List<ArrayList<StationNode>> graph = new ArrayList<>();
-        int[] d = new int[n+1];
+        int[] d = new int[n + 1];
         Arrays.fill(d, Integer.MAX_VALUE);
 
-        for(int i = 0; i <= n; i++) {
+        for (int i = 0; i <= n; i++) {
             graph.add(new ArrayList<>());
         }
 
-        for(Station subway : subways) {
+        for (Station subway : subways) {
             int value;
             switch (type) {
                 case "distance":
@@ -48,24 +62,24 @@ public class StationService {
         }
 
         PriorityQueue<StationNode> pq = new PriorityQueue<>();
-        pq.offer(new StationNode(start, 0));
-        d[start] = 0;
+        pq.offer(new StationNode(startSubwayStation.getStationID(), 0));
+        d[startSubwayStation.getStationID()] = 0;
 
-        while(!pq.isEmpty()) {
+        while (!pq.isEmpty()) {
             StationNode node = pq.poll();
             int dist = node.getDistance();
             int now = node.getStation();
 
-            if(d[now] < dist) continue;
-            for(int i = 0; i < graph.get(now).size(); i++) {
+            if (d[now] < dist) continue;
+            for (int i = 0; i < graph.get(now).size(); i++) {
                 int cost = d[now] + graph.get(now).get(i).getDistance();
-                if(cost < d[graph.get(now).get(i).getStation()]) {
+                if (cost < d[graph.get(now).get(i).getStation()]) {
                     d[graph.get(now).get(i).getStation()] = cost;
                     pq.offer(new StationNode(graph.get(now).get(i).getStation(), cost));
                 }
             }
         }
 
-        return d[end];
+        return d[endSubwayStation.getStationID()];
     }
 }
